f1a0d5df086e3a7e6788d2f0c61c4228ddb81395
raimbekov789 <raimbekov.jahongir@mail.ru>
2020-04-06 12:33:20 +0500

    Минимальное оставное дерево

---
+++ stations.py
@@ -0,0 +1,30 @@
+N = 6
+INF = 30000
+W = [
+    [0, 2, 4, INF, INF, INF],
+    [2, 0, 9, 7, INF, INF],
+    [4, 9, 0, 8, 1, INF],
+    [INF, 7, 8, 0, 3, 1],
+    [INF, INF, 1, 3, 0, 2],
+    [INF, INF, INF, 1, 2, 0]
+]
+
+col = [i for i in range(N)]
+
+ostov = []
+for k in range(N-1):
+    min = 1e10
+    for i in range(N):
+        for j in range(N):
+            if col[i] != col[j] and W[i][j] < min:
+                iMin = i
+                jMin = j
+                min = W[i][j]
+    ostov.append((iMin, jMin))
+    c = col[jMin]
+    for i in range(N):
+        if col[i] == c:
+            col[i] = col[iMin]
+
+for a in ostov:
+    print(chr(a[0]+65), chr(a[1]+65), sep='')
